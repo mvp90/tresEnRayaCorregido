b9c6967ed7e1dc768bd1613fc393052910014b15
miguel verdejo peñuela <miguel verdejo peuela>
2016-11-21 11:46:52 +0100

    tres en raya chungo

--- src/TresEnRaya.java
+++ src/TresEnRaya.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+
+
 public class TresEnRaya {
 
 	private int contador;
@@ -16,15 +18,15 @@ public class TresEnRaya {
             System.out.println ("Volver a jugar ('si'/'no')? ");
             Scanner in =new Scanner(System.in);
             ch=in.nextLine();
-            System.out.println("ch valor es  "+ch);
-        }while (ch.equals("no"));
-        
-        
+            ch=ch.toUpperCase(); 
+            // System.out.println("ch valor es  "+ch); ####ERRRRROOOOORRR  No es necesario imprimir nada. 
+        }while (ch.equals("SI")); 
+        //####### ERRRRROOOOORRRR   Si el usuario introduce si, salta error.
     }
     public  void newBoard()
     {
         
-        char posndef[] = {'0','1', '3', '2', '4', '5', '7', '6','8', '9'}; 
+        char posndef[] = {'0','1', '2', '3', '4', '5', '6', '7','8', '9'}; // #####EEERRRROOOOORRRR  los elementos no estaban bien vectorizados.  
         int i;
         contador = 0;
         player = 'X';
@@ -37,13 +39,13 @@ public class TresEnRaya {
     {
         System.out.println( "\n\n" );
         System.out.println(  "\n\n" );
-        System.out.println(  "\n\n\t\t" + posn [2] + "   | " +posn [3]+ "  | " +posn [3]);
+        System.out.println(  "\n\n\t\t" + posn [1] + "   | " +posn [2]+ "  | " +posn [3]);   // ####ERRRROOOORRRRR impresión en orden correcto de las posiciones del vector 
         System.out.println(  " \t\t    |    |   " );
         System.out.println(  " \t\t ___|____|___ " );
-        System.out.println(  "\n\n\t\t" +posn [4]+ "   | " +posn [5]+ "  | " +posn [7]);
+        System.out.println(  "\n\n\t\t" +posn [4]+ "   | " +posn [5]+ "  | " +posn [6]); 
         System.out.println(  " \t\t    |    |   " );
         System.out.println(  " \t\t ___|____|___ " );
-        System.out.println(  "\n\n\t\t" +posn [1]+ "   | " +posn [8]+ "  | " +posn [9]);
+        System.out.println(  "\n\n\t\t" +posn [7]+ "   | " +posn [8]+ "  | " +posn [9]);
         System.out.println(  " \t\t    |    |   " );
         System.out.println(  " \t\t    |    |   " );
         System.out.println(  "\n\n" );
@@ -56,14 +58,15 @@ public class TresEnRaya {
         char blank = ' ';
         
         System.out.println(  "Jugador " + getPlayer() +" va a jugar con ficha 'X'" );
-        
-        do {
-            tablero();              // display current board
+                do {
+            // tablero();              //######## ERRORRRR Me imprime mas de una vez el tablero.
             
             System.out.println(  "\n\n Jugador " + getPlayer() +" elije la posicion." );
             
-            boolean posTaken = false;
+            boolean posTaken = true;
+            
             while (posTaken) {
+           
                 Scanner in =new Scanner (System.in);
                 spot=in.nextInt();
                 posTaken = comprobarPosn(spot);
@@ -73,7 +76,7 @@ public class TresEnRaya {
             
             System.out.println(  "Buen movimiento." );
             
-            tablero();              // display current board
+            tablero();             
             
             nextPlayer();
         }while ( comprobarGanador() == blank );
@@ -84,15 +87,15 @@ public class TresEnRaya {
     {
         char Winner = ' ';
         
-        // Comprobacion sobre X
-        if (posn[1] == 'X' && posn[4] == 'X' && posn[3] == 'X') Winner = 'X';
-        if (posn[4] == 'X' && posn[1] == 'X' && posn[6] == 'X') Winner = 'X';
-        if (posn[7] == 'X' && posn[5] == 'X' && posn[9] == 'X') Winner = 'X';
-        if (posn[1] == 'X' && posn[3] == 'X' && posn[7] == 'X') Winner = 'X';
-        if (posn[2] == 'X' && posn[6] == 'X' && posn[8] == 'X') Winner = 'X';
-        if (posn[3] == 'X' && posn[5] == 'X' && posn[9] == 'X') Winner = 'X';
-        if (posn[1] == 'X' && posn[3] == 'X' && posn[9] == 'X') Winner = 'X';
-        if (posn[3] == 'X' && posn[2] == 'X' && posn[7] == 'X') Winner = 'X';
+       
+        if (posn[1] == 'X' && posn[4] == 'X' && posn[7] == 'X') Winner = 'X'; // ERROR No cogía ordenados las elementos del vector.
+        if (posn[2] == 'X' && posn[5] == 'X' && posn[8] == 'X') Winner = 'X';
+        if (posn[3] == 'X' && posn[6] == 'X' && posn[9] == 'X') Winner = 'X';
+        if (posn[1] == 'X' && posn[2] == 'X' && posn[3] == 'X') Winner = 'X';
+        if (posn[4] == 'X' && posn[5] == 'X' && posn[6] == 'X') Winner = 'X';
+        if (posn[7] == 'X' && posn[8] == 'X' && posn[9] == 'X') Winner = 'X';
+        if (posn[1] == 'X' && posn[5] == 'X' && posn[9] == 'X') Winner = 'X';
+        if (posn[3] == 'X' && posn[5] == 'X' && posn[7] == 'X') Winner = 'X';
         if (Winner == 'X' )
         {System.out.println("Jugador 1 ha ganado" );
             return Winner;
@@ -112,7 +115,7 @@ public class TresEnRaya {
             System.out.println( "Jugador 2 ha ganado" );
         return Winner; }
         
-        // comprobar empate
+       
         for(int i=1;i<11;i++)
         {
             if(posn[i]=='X' || posn[i]=='O')
@@ -137,7 +140,7 @@ public class TresEnRaya {
     {
         
         
-        if (posn[spot] == 'X' && posn[spot] == 'O')
+        if (posn[spot] == 'X' || posn[spot] == 'O')  //  ####ERRRROOOOORRR Necesarias tuberias para correcto sentido.
         {
             System.out.println("Esa posicion ya se ha elegido, por favor elija otra");
             return true;
@@ -153,8 +156,10 @@ public class TresEnRaya {
     public  void nextPlayer()
     {
         if (player == 'X')
-        player = 'X';
-        else player = 'O';
+        // player = 'X'; 
+        player = 'O'; //###### ERRRRRRROOORRRR SI TERMINA X ES TURNO DE O.
+        //else player = 'O';
+        else player ='X'; // Al contrario tabién. 
         
     }
     
@@ -168,3 +173,4 @@ public class TresEnRaya {
         return player;
     }
 }
+/// MIGUEL VERDEJO PEÑUELA
